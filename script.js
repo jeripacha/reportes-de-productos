04097f5e4c23a63b3eae3e8be65f736e94c42e8f
Pacha sunset <carlosberrocal12345@gmail.com>
2026-03-17 00:04:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const tableBody = document.getElementById("table-body");
 const tableHead = document.getElementById("table-head");
 
 const productosBase = [
-  "Fernet Buhero","Fernet Branca","havana especial","havana 7 años","T olmeca chocolate","T olmeca reposado","Flor de caña 7 años","Flor de caña 5 años",
+  "Fernet Buhero","Fernet Branca","havana especial","havana especial 1L","havana 7 años","T olmeca chocolate","T olmeca reposado","Flor de caña 7 años","Flor de caña 5 años",
   "Ron Havana","Beefeater Pink","Gyn Andina","Gyn Frutilla","Gyn Amazónico","Gyn Frutos del Bosque",
   "Cerveza Huari","Cerveza Budweiser","Cerveza Corona","Latas Paceña","Golden",
   "Viuda","Parrales","Agua Pacha","Agua Vital Normal","Agua Vital con Gas",
